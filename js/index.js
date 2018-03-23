652729b01bdcc1921154513293c5b03f7abee634
0popi <37343166+0popi@users.noreply.github.com>
2018-03-23 15:53:47 +0200

    initial

---
+++ js/index.js
@@ -0,0 +1,26 @@
+;(function () {
+    let card  = document.getElementsByClassName('card')[0],
+        moved = 0,
+        interval;
+
+    if (!card) return;
+    
+    card.addEventListener('click', function (event) {
+        clearInterval(interval);
+        card.style.transform = '';
+        
+        .
+        if (event.target.nodeName === 'A') {
+            return;
+        }
+        
+        let cName   = card.getAttribute('data-toggle-class');
+        let toggled = card.classList.contains(cName);
+        card.classList[toggled ? 'remove' : 'add'](cName);
+    });
+    
+    interval = setInterval(function () {
+        moved = moved ? 0 : 10;
+        card.style.transform = 'translateY(' + moved + 'px)';
+    }, 1500);
+})();
